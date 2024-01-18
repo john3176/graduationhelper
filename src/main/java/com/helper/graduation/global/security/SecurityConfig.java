9f42717135fbadf657dcbdbf44f250f743ff2914
하늘선 <john3176@hotmail.com>
2024-01-18 16:32:09 +0900

    Added config for h2-console

--- src/main/java/com/helper/graduation/global/security/SecurityConfig.java
+++ src/main/java/com/helper/graduation/global/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,7 +16,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll());
+                        .anyRequest().permitAll())
+                .headers(
+                        httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(
+                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                        )
+                );
 
         return http.build();
 
